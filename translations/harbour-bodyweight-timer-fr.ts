92f0f87bcf02d8e037050c5c5da2a700943795c2
Robert Scheinpflug <r.scheinpflug@posteo.de>
2018-03-16 21:01:09 +0000

    Added translation using Weblate (French)

---
+++ translations/harbour-bodyweight-timer-fr.ts
@@ -0,0 +1,404 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>about</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="30"/>
+        <location filename="../qml/pages/About.qml" line="53"/>
+        <source>made by %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="35"/>
+        <source>source code</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="49"/>
+        <source>icon</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="63"/>
+        <source>credits</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CircleInterval</name>
+    <message>
+        <location filename="../qml/pages/CircleInterval.qml" line="53"/>
+        <source>settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CircleInterval.qml" line="91"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CircleInterval.qml" line="137"/>
+        <source>pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CircleInterval.qml" line="140"/>
+        <source>resume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CircleInterval.qml" line="143"/>
+        <source>start</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="183"/>
+        <source>time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="207"/>
+        <source>set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="241"/>
+        <source>exercise</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ExerciseSettings</name>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="29"/>
+        <source>duration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="32"/>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="37"/>
+        <source>duration per set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="33"/>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="38"/>
+        <source>sets per exercise</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="34"/>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="39"/>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="43"/>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="49"/>
+        <source>number exercises</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="42"/>
+        <source>duration per exercise</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="46"/>
+        <source>rounds per exercise</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="47"/>
+        <source>duration of active time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="48"/>
+        <source>duration of pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="83"/>
+        <source>reset to default</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="93"/>
+        <source>settings: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="106"/>
+        <source>Do as much as you can for the duration of the exercise.
+
+Reduce pauses to a minimum.
+
+Tripple bell = end&quot;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="108"/>
+        <source>In a 4 minute set do the first superset (a pair of 2 exercises).
+Do at repetition 1 to 5 the first pair-exercise, and at repetition 6 to 12 the second pair-exercise. 
+First pair-exercise should not cause you musle malfunction.
+
+Single bell = next set
+Double bell = next exercise pair
+Tripple bell = end</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="110"/>
+        <source>In a 3 minute set do 6 to 12 repetitions (stop on muscle malfunction). Pause rest of the set.
+
+1 of 3 sets should cause you to muscle malfunction. Do harder/another exercise if not.
+
+Single bell = next set
+Double bell = next exercise
+Tripple bell = end</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="112"/>
+        <source>Do 1 repetition of an exercise and pause the time it took you do to so. Then do 2 repetitions and pause the time it took you to do these 2. And so forth.
+On muscle malfunction reduce the repetitions by 1, then by another and so forth.
+
+Already at 1 repetition again and time is not over? Start a new ladder!
+
+Single bell = next exercise
+Tripple bell = end</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="114"/>
+        <source>During active time (green) move on. During pause time (red) pause.
+
+Try to find your ideal tempo (consistent repetitions).
+
+Single bell = active time begins
+Double bell = pause time begins
+Tripple bell = next exercise or end.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="118"/>
+        <source>instruction</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExerciseSettings.qml" line="149"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Home</name>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="32"/>
+        <source>about</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="40"/>
+        <source>workouts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="48"/>
+        <source>circle interval</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="52"/>
+        <source>interval set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="60"/>
+        <source>super set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="56"/>
+        <source>ladder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Home.qml" line="64"/>
+        <source>tabata</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>IntervalSet</name>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="67"/>
+        <source>settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="105"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="168"/>
+        <location filename="../qml/pages/IntervalSet.qml" line="171"/>
+        <source>current set: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="174"/>
+        <source>sets for each exercise: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="186"/>
+        <source>current exercise: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="201"/>
+        <source>pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="205"/>
+        <source>resume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/IntervalSet.qml" line="208"/>
+        <source>start</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Ladder</name>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="52"/>
+        <source>settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="89"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="147"/>
+        <location filename="../qml/pages/Ladder.qml" line="150"/>
+        <source>current exercise: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="165"/>
+        <source>pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="169"/>
+        <source>resume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ladder.qml" line="172"/>
+        <source>start</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SuperSet</name>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="67"/>
+        <source>settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="102"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="165"/>
+        <location filename="../qml/pages/SuperSet.qml" line="168"/>
+        <source>current set: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="171"/>
+        <source>sets for each exercise: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="183"/>
+        <source>current exercise: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="185"/>
+        <source>number of exercises: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="198"/>
+        <source>pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="202"/>
+        <source>resume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SuperSet.qml" line="205"/>
+        <source>start</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Tabata</name>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="107"/>
+        <source>settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="144"/>
+        <source>%1m %2s</source>
+        <extracomment>m = minute, s = second</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="230"/>
+        <source>current exercise: %1 of %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="246"/>
+        <source>pause</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="250"/>
+        <source>resume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Tabata.qml" line="253"/>
+        <source>start</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
